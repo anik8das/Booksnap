f42dd4db85d31ea71f3d6f94d97b30cad7a174f5
Aniket Das <aniketdas001@gmail.com>
2023-03-28 23:19:40 +0530

    Linked input field to firebase

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Firestore, getFirestore } from '@angular/fire/firestore';
 import { MatDialog } from '@angular/material/dialog';
+import { addDoc, collection } from 'firebase/firestore';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +10,19 @@ import { MatDialog } from '@angular/material/dialog';
 })
 export class AppComponent {
   title = 'frontend';
-  constructor(public dialog: MatDialog) {}
+  firestore = inject(Firestore);
+  db = getFirestore(this.firestore.app);
+  constructor(public dialog: MatDialog) {
+    this.db = getFirestore(this.firestore.app);
+  }
+
+  async subscribe(email: string): Promise<void> {
+    console.log(email);
+    const subscriberCollection = collection(this.db, 'subscribers');
+    await addDoc(subscriberCollection, { email });
+    this.openDialog();
+  }
+
   openDialog(): void {
     const dialogRef = this.dialog.open(DialogOverviewExampleDialog);
     dialogRef.afterClosed().subscribe((result) => {
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { environment } from '../environments/environment';
 import { AppComponent, DialogOverviewExampleDialog } from './app.component';
 
 @NgModule({
@@ -13,6 +16,8 @@ import { AppComponent, DialogOverviewExampleDialog } from './app.component';
     BrowserAnimationsModule,
     MatButtonModule,
     MatDialogModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent],
