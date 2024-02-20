d3f3caca45263adcda1e8ea6b4d2683d5ab9f9a9
Aniket Das <aniketdas001@gmail.com>
2024-02-19 17:15:07 -0800

    add option for book categories

--- frontend/src/app/subscribe/subscribe.component.ts
+++ frontend/src/app/subscribe/subscribe.component.ts
@@ -1,7 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { Firestore, getFirestore } from '@angular/fire/firestore';
 import { MatDialog } from '@angular/material/dialog';
-import { addDoc, collection, getDocs, query, where } from 'firebase/firestore';
+import {
+  addDoc,
+  collection,
+  getDocs,
+  query,
+  updateDoc,
+  where,
+} from 'firebase/firestore';
 
 @Component({
   selector: 'app-subscribe',
@@ -12,26 +19,51 @@ export class SubscribeComponent {
   firestore = inject(Firestore);
   db = getFirestore(this.firestore.app);
   loading: boolean = false;
+  category: string = 'random';
   constructor(public dialog: MatDialog) {
     this.db = getFirestore(this.firestore.app);
   }
 
   async subscribe(email: string): Promise<void> {
+    if (!this.isValidEmail(email)) {
+      alert('Please enter a valid email address');
+      return;
+    }
     this.loading = true;
     const subscriberCollection = collection(this.db, 'subscribers');
     const q = query(subscriberCollection, where('email', '==', email));
     const querySnapshot = await getDocs(q);
     let dialogRef = null;
     if (querySnapshot.size > 0) {
-      dialogRef = this.dialog.open(AlreadySubscribedModal);
+      // user is already subscribed, checked for category
+      querySnapshot.forEach(async (doc) => {
+        if (doc.data()['category'] !== this.category) {
+          await updateDoc(doc.ref, { category: this.category });
+          dialogRef = this.dialog.open(CategoryUpdatedModal);
+        } else {
+          dialogRef = this.dialog.open(AlreadySubscribedModal);
+        }
+      });
     } else {
-      await addDoc(subscriberCollection, { email });
+      await addDoc(subscriberCollection, { email, category: this.category });
       dialogRef = this.dialog.open(SignupConfirmationModal);
     }
     this.loading = false;
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log('The dialog was closed');
-    });
+    if (dialogRef) {
+      dialogRef.afterClosed().subscribe((result) => {
+        console.log('The dialog was closed');
+      });
+    }
+  }
+
+  isValidEmail(email: string) {
+    // Regular expression for validating email addresses
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+
+  selectCategory(category: string): void {
+    this.category = category;
   }
 
   openDialog(): void {
@@ -53,3 +85,9 @@ export class SignupConfirmationModal {}
   templateUrl: '../modals/alreadySubscribed.html',
 })
 export class AlreadySubscribedModal {}
+
+@Component({
+  selector: 'category-updated-modal',
+  templateUrl: '../modals/categoryUpdated.html',
+})
+export class CategoryUpdatedModal {}
